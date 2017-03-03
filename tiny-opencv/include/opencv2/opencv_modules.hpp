82ef7596eef0fd2c19939224ba5c473087a59009
conansherry <conansherry@163.com>
2017-03-03 11:13:11 +0800

    test core success.

--- tiny-opencv/include/opencv2/opencv_modules.hpp
+++ tiny-opencv/include/opencv2/opencv_modules.hpp
@@ -10,12 +10,12 @@
 // For example, patented algorithms for non-profit/non-commercial use only.
 /* #undef OPENCV_ENABLE_NONFREE */
 
-#define HAVE_OPENCV_CALIB3D
+//#define HAVE_OPENCV_CALIB3D
 #define HAVE_OPENCV_CORE
-#define HAVE_OPENCV_FEATURES2D
-#define HAVE_OPENCV_FLANN
-#define HAVE_OPENCV_IMGPROC
-#define HAVE_OPENCV_ML
-#define HAVE_OPENCV_OBJDETECT
+//#define HAVE_OPENCV_FEATURES2D
+//#define HAVE_OPENCV_FLANN
+//#define HAVE_OPENCV_IMGPROC
+//#define HAVE_OPENCV_ML
+//#define HAVE_OPENCV_OBJDETECT
 
 
